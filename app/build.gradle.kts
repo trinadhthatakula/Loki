f8b30b067bbeede73ffdeb27aebf788304c28e5d
trinadh thatakula <trinadh.thatakula@gmail.com>
2025-07-17 19:23:45 +0530

    Refactor: Organize dependencies in build.gradle.kts
    - Grouped dependencies in `app/build.gradle.kts` by category (Core, Splash Screen, Compose, Lifecycle, Test, Kotlinx Serialization, Drawable Painter, LibSU, Koin, Shizuku).
    - No functional changes were made to the dependencies themselves.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,15 +51,24 @@ android {
 
 dependencies {
 
+    /// Core
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    /// Splash Screen
+    implementation(libs.androidx.splashscreen)
+
+    /// Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.splashscreen)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    /// Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    /// Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -68,15 +77,20 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    /// Kotlinx Serialization
     implementation(libs.kotlinx.serialization.json)
 
+    /// Drawable Painter
     implementation(libs.accompanist.drawablepainter)
 
+    /// LibSU
     implementation(libs.topjohnwu.libsu.core)
 
+    /// Koin
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
 
+    /// Shizuku
     implementation(libs.shizuku.api)
     implementation(libs.shizuku.provider)
 
